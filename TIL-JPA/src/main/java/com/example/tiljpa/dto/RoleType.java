bdefa7d0b0050dd1e20b245c8436077de68b94d0
jhva <junghun8158@naver.com>
2023-08-28 22:30:34 +0900

    feat: init

--- TIL-JPA/src/main/java/com/example/tiljpa/dto/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.tiljpa.dto;
-
-public enum RoleType {
-
-    ADMIN, USER
-}
--- TIL-JPA/src/main/java/com/example/tiljpa/entity/Album.java
+++
@@ -1,7 +0,0 @@
-package com.example.tiljpa.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Album extends Item {
-}
